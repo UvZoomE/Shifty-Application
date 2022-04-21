a64dd6761bb460239db6ff3cf951f65ba73db0e4
devonknudsen <devonknudsen16@gmail.com>
2022-04-21 11:40:45 -0500

    added port switch for index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 const server = require('./app.js');
 const PORT = 3001;
 
-server.listen(PORT, () => {
+server.listen((process.env.PORT || PORT), () => {
   console.log(`Listening on port ${PORT}`)
 })
